cb69c0266d8eba1b6ea9282b39cc88697eb24c8f
Jet Wardenier <jet.wardenier@joinseven.nl>
2025-05-16 17:42:18 +0200

    if theme not known generate summaries

--- resources/search.py
+++ resources/search.py
@@ -4,7 +4,7 @@ from flask_smorest import Blueprint, abort
 from flask_jwt_extended import jwt_required
 from flask.views import MethodView
 from schemas import PlainDocumentSchema, SearchDocumentsSchema, SearchObjectsSchema
-from .resource_classes import ChunkSearchingClass, CL_Mistral_Embeddings
+from .resource_classes import ChunkSearchingClass, CL_Mistral_Embeddings, CL_Mistral_Completions
 
 blp = Blueprint("Search", "search", description="Operations on the search page")
 
@@ -15,7 +15,6 @@ class SearchDocuments(MethodView):
 
     @jwt_required()
     @blp.arguments(SearchDocumentsSchema)
-    # @blp.response(200, PlainDocumentSchema(many=True))
     @blp.response(200, SearchObjectsSchema(many=True))
     def post(self, input_data):
         """Gets the first 10 documents it can find in the OpenSearch index"""
@@ -25,15 +24,33 @@ class SearchDocuments(MethodView):
             "embedding": CL_Mistral_Embeddings().generate_embedding(search_string)
         }
         objects = ChunkSearchingClass().search_documents(search_config)
-        
+        print("Objects: ", objects)
         # Aggregate all document IDs into a single list
         all_document_ids = [
             doc['document_id'] 
             for entry in objects 
             for doc in entry['documents']
         ]
-        # Add the list of document IDs to the first entry
+        
         if objects:
             objects[0]['document_ids'] = all_document_ids
+
+        # Generate summaries if the search string is not "RijnlandRoute"
+        if search_string != "RijnlandRoute":
+            summaries = []
+            for entry in objects[:1]:
+                doc_count = 0
+                for doc in entry['documents']:
+                    if doc_count >= 3:
+                        break
+                    content_text = doc['content_text']
+                    print("Content text: ", content_text)
+                    print("Docid: ", doc['document_id'])
+                    prompt = f"Geef een samenvatting van de volgende tekst: {content_text} over het thema {search_string}. Beschrijf kort wat de kern van de tekst is en wees concreet. Verzin geen zaken erbij. Begin je tekst NIET met 'De tekst beschrijft' of 'de inhoud van de tekst' zorg dat het een vloeiende tekst is. Deze tekst is bedoeld voor Statenleden, dus bepaald jargon over overheidstermologie mag gebruikt worden. Beperk je tot maximaal 4 a 5 zinnen. Vermijd vage en onnodige zinnen. Benoem alleen relevante zaken, als je echt niks inhoudelijk kan vinden, geef dat dan in één zin aan en omschrijf gewoon het onderwerp RijnlandRoute."
+                    
+                    summary = CL_Mistral_Completions().generate_summary(prompt)
+                    print("Generating summary for document: ", doc)
+                    doc['summary'] = summary
+                    doc_count += 1
 
         return objects
